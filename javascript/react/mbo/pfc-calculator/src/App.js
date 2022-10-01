612eccf8d05ab60d179af6f88aa5eaeb06d383b2
yo0o0y <yoyoooi.1414@gmail.com>
2022-10-01 17:05:17 +0900

    PFCの表を追加

--- javascript/react/mbo/pfc-calculator/src/App.js
+++ javascript/react/mbo/pfc-calculator/src/App.js
@@ -9,6 +9,11 @@ class App extends React.Component {
       sex: 'female',
       weight: 50,
       fatPercentage: 25,
+      pfc: {
+        p: { name: 'たんぱく質', g:100, kcal: 400 },
+        f: { name: '脂質', g:20, kcal: 180 },
+        c: { name: '炭水化物', g:230, kcal: 920 },
+      },
     };
   }
 
@@ -22,14 +27,42 @@ class App extends React.Component {
           <li>体重：{this.state.weight}kg</li>
           <li>体脂肪率：{this.state.fatPercentage}%</li>
           <li>徐脂肪体重：{this.calcLeanBodyMass()}kg</li>
+          <li>基礎代謝：{this.calcBm()}kcal</li>
         </ul>
+        <table>
+          <tr>
+            <th></th>
+            <th>g</th>
+            <th>kcal</th>
+          </tr>
+          { Object.keys(this.state.pfc).map((key)=>{
+            return (
+              <tr>
+                <th>{this.state.pfc[key].name}</th>
+                <td>{this.state.pfc[key].g} g</td>
+                <td>{this.state.pfc[key].kcal} kcal</td>
+              </tr>
+            )
+          })}
+        </table>
       </div>
     );
   }
 
+  /*
+   * 徐脂肪体重を計算
+   * TODO: computed的なんにしたい
+   */
   calcLeanBodyMass() {
-    // TOOD: computed的なんにしたい
     return this.state.weight * (1 - this.state.fatPercentage / 100);
+  }
+
+  /*
+   * 基礎代謝を計算
+   * TODO: computed的なんにしたい
+   */
+  calcBm() {
+    return this.state.weight * 30;
   }
 
 }
