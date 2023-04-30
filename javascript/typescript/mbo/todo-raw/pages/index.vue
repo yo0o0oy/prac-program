f12a05254d7578b01200052cfbb3288e4e1d0adc
yo0o0y <yoyoooi.1414@gmail.com>
2023-04-30 15:35:23 +0900

    first commit

---
+++ javascript/typescript/mbo/todo-raw/pages/index.vue
@@ -0,0 +1,45 @@
+<template lang="pug">
+.page-top
+  h1 買い物リスト
+  ul
+    li(v-for="todo, i in todos" :class="todo.status")
+      .check x
+      input(type="text" v-model="todos[i].task")
+    li.add
+      input(type="text" v-model="newTodo")
+      button 追加
+</template>
+
+<script>
+export default {
+  data: () => ({
+    todos: [
+      {
+        status: 'not-yet',
+        task: 'にんじん',
+      },
+      {
+        status: 'done',
+        task: 'にんじん',
+      },
+    ],
+    newTodo: '',
+  }),
+}
+</script>
+
+<style lang="stylus">
+.page-top
+  ul
+    li
+      display flex
+      gap 10px
+      align-items center
+      &.done
+        .check
+          color green
+        input
+          text-decoration line-through
+          opacity 0.5
+          pointer-events none
+</style>
