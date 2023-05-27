5f804f4dfe4d18ec88d90b00d2b25662125b3ee0
yo0o0y <yoyoooi.1414@gmail.com>
2023-05-27 21:12:52 +0900

    ヘッダーコンポーネント追加

---
+++ javascript/typescript/mbo/todo-ts/components/TdrHeader.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+header.header
+  img.logo(src="images/logo.png")
+  span TODOLIST
+</template>
+
+<style lang="stylus">
+.header
+  height 60px
+  position sticky
+  top 0
+  z-index 10
+  background-color color-main
+  display flex
+  align-items center
+  justify-content center
+  gap 8px
+  .logo
+    max-height 50%
+    margin-top -5px
+  span
+    color white
+    font-size 24px
+    font-family-arial()
+</style>
---
+++ javascript/typescript/mbo/todo-ts/layouts/default.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+.layout-default
+  TdrHeader
+  .main
+    Nuxt
+</template>
+
+<script>
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'LayoutDefault',
+})
+</script>
